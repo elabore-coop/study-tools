3b1800a6bf47e32db62d2a95549281ae8659dfa2
Boris Gallet <boris.gallet@gmail.com>
2025-07-08 16:14:12 +0200

    [FIX] studies_base: change correct date and name for questionnaire

--- studies_base/models/study_questionnaire.py
+++ studies_base/models/study_questionnaire.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import api, fields, models, _
-from odoo import osv
-from odoo.exceptions import UserError
+from odoo import api, fields, models
 
 
 class StudyQuestionnaire(models.Model):
@@ -35,11 +33,34 @@ class StudyQuestionnaire(models.Model):
     copyright = fields.Text("Copyright")
     copyright_label = fields.Char("Propriétaire et année du copyright")
 
-    created = fields.Datetime("Created")
-    date = fields.Datetime("Date")
+    created = fields.Datetime("Created", compute="_compute_created", readonly=True)
+    date = fields.Datetime("Date", compute="_compute_updated", readonly=True)
+
+    @api.depends("create_date")
+    def _compute_created(self):
+        for record in self:
+            if not record.created:
+                record.created = record.create_date
+
+    @api.depends("write_date")
+    def _compute_updated(self):
+        for record in self:
+            record.date = record.write_date
 
     active = fields.Boolean("Actif", default=True)
 
     def copy(self, default=None):
         default = dict(default or {}, identifier_primary_id=None)
         return super().copy(default)
+
+    @api.depends("title", "name")
+    def name_get(self):
+        result = []
+        for questionnaire in self:
+            if not questionnaire.name:
+                result.append((questionnaire.id, questionnaire.title))
+            else:
+                result.append(
+                    (questionnaire.id, f"[{questionnaire.name}] {questionnaire.title}")
+                )
+        return result
